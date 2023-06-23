bc3fc8b7cd8397acc379323c36cc2fcbd31226b4
Kielancia <mysweetnaivety@gmail.com>
2023-06-23 16:30:35 +0200

    Update main class

--- NWPO_Proj/src/main/java/org/example/Main.java
+++ NWPO_Proj/src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import collectingData.ImportData;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<IqCountry> iqCountryList = ImportData.importPeopla();
+        int i = 1;
+        Iterator var = iqCountryList.iterator();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner console = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Podaj dolny zakres przedzialu iq: ");
+        int p1 = console.nextInt();
+        System.out.print("Podaj gorny zakres przedzialu iq: ");
+        int p2 = console.nextInt();
+// Na 5
+        System.out.println("Kraje dla zakresu od " + p1 + " do " + p2 + ": ");
+        for (IqCountry ic : iqCountryList) {
+            if (ic.iq >= p1 && ic.iq <= p2)
+                System.out.println(ic.place + "..." + ic.country + "..." + ic.iq);
         }
+// Na 4
+		/*System.out.println("\nPelna lista krajow: ");
+		while(var.hasNext()) {
+			IqCountry ic = (IqCountry)var.next();
+			int temp = i++;
+			System.out.println(ic.place + "..." + ic.country + "..." + ic.iq);
+		}*/
     }
 }
